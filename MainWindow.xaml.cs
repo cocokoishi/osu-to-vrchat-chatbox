1c2facc732e4111cb8793baa5504ebaa7d0d6872
cocokoishi <herobrine980808@gmail.com>
2026-04-26 17:38:35 +0800

    feat: add clickable project links at bottom of Advanced tab

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -367,5 +367,11 @@ namespace OsuOscVRC
             DoStop();
             base.OnClosing(e);
         }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            e.Handled = true;
+        }
     }
 }
